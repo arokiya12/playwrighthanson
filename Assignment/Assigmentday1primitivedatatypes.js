dd3e74d4bd91c63468f10af5e5080921fb1967c8
arokiya12 <arokiyavinnarasi@gmail.com>
2026-01-23 14:08:06 +0530

    day 1 and day 2 Playwright assignment
    day 1 and day 2 Playwright assignment

---
+++ Assignment/Assigmentday1primitivedatatypes.js
@@ -0,0 +1,11 @@
+var userName='testleaf'
+var organizantion='testleaforg'
+var contactN0=478596354
+var knowsAutomation =true
+var usesPlaywright
+console.log("userName:"+userName)
+console.log( 'organizantion :', organizantion) 
+console.log(`contactN0:+`, contactN0) 
+ console.log("knowsAutomation" + knowsAutomation)
+ console.log(`usesPlaywright:+`, usesPlaywright) 
+ 
---
+++ Assignment/Day2DataType.js
@@ -0,0 +1,12 @@
+var idNo=8964
+var comName='Testleaf'
+var status=true
+var aadharNo=44445789622321112554632n
+var empName
+var emp=null
+console.log("Emp No :"+idNo)
+console.log( 'comName :', comName) 
+console.log(`status:+`, status) 
+ console.log("aadharNo" + aadharNo+""+typeof(aadharNo) )
+ console.log(`empName:+`, empName) 
+ console.log("emp" + emp )
