31f18b8fcb88b67b75036bb51cfef3b6a8256de5
arokiya12 <arokiyavinnarasi@gmail.com>
2026-01-27 11:41:41 +0530

    Add files via upload
    DAY 2 assignment

---
+++ day2numbertype.js
@@ -0,0 +1,8 @@
+let number = 0
+if(number>0){
+    console.log("positive")
+}else if(number<0){
+    console.log("negative")
+}else(number==0)
+    console.log("number is zero")
+
---
+++ day2oddoreven.js
@@ -0,0 +1,7 @@
+let evennum=3
+if(evennum%2==0){
+console.log("even number")
+}else{
+    console.log("odd number")
+}
+
